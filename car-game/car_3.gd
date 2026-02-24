01684ca19824f58259ec8bf99c83ceb3fb98959d
ZyunRei06 <marteljhunrey@smcbi.edu.ph>
2026-02-24 11:03:02 +0800

    Add files via upload

---
+++ car-game/car_3.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+@export var speed: float = 250
+
+func _physics_process(delta):
+	velocity = Vector2(0, speed) # move downward
+	move_and_slide()
+
+	# Remove car when off screen
+	if position.y > get_viewport_rect().size.y + 100:
+		queue_free()
